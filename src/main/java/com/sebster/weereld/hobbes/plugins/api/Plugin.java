9265657e1ab4de427c9684ef0d2842696671ac8c
Sebastiaan van Erk <sebster@sebster.com>
2016-11-02 18:25:57 +0100

    Added @FunctionalInterface annotation.

--- src/main/java/com/sebster/weereld/hobbes/plugins/api/Plugin.java
+++ src/main/java/com/sebster/weereld/hobbes/plugins/api/Plugin.java
@@ -2,6 +2,7 @@ package com.sebster.weereld.hobbes.plugins.api;
 
 import com.sebster.telegram.api.data.messages.TelegramMessage;
 
+@FunctionalInterface
 public interface Plugin {
 	
 	void receiveMessage(TelegramMessage telegramMessage);
