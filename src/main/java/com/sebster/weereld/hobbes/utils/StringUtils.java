5cb4310c6c73a3c81f4db7ab3e82a958777d5f84
Sebastiaan van Erk <sebster@sebster.com>
2016-11-02 09:44:21 +0100

    Added utility method for formatting values if they are not null.

--- src/main/java/com/sebster/weereld/hobbes/utils/StringUtils.java
+++ src/main/java/com/sebster/weereld/hobbes/utils/StringUtils.java
@@ -1,10 +1,16 @@
 package com.sebster.weereld.hobbes.utils;
 
+import static java.lang.String.format;
+
 import java.util.Optional;
 
 public class StringUtils {
 	
 	private StringUtils() {
+	}
+
+	public static String formatIfNotNull(Object value, String format) {
+		return value != null ? format(format, value) : "";
 	}
 
 	public static String formatIfPresent(Optional<?> optional, String format) {
