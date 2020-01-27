2520e8ba0afaf155c7dd4b892f8bf2a2d63a7187
Sebastiaan van Erk <sebster@sebster.com>
2020-01-27 19:21:23 +0100

    Add autoconfiguration of telegram implementation.

---
+++ telegram-impl/src/main/java/com/sebster/telegram/impl/TelegramServiceAutoConfiguration.java
@@ -0,0 +1,20 @@
+package com.sebster.telegram.impl;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.sebster.telegram.api.TelegramService;
+
+@Configuration
+@EnableConfigurationProperties(TelegramProperties.class)
+public class TelegramServiceAutoConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean
+	public TelegramService telegramService(TelegramProperties properties) {
+		return new TelegramServiceImpl(properties.getAuthKey());
+	}
+
+}
