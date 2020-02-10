5604d035015d0d46ec485186bb550070eadf3349
Sebastiaan van Erk <sebster@sebster.com>
2020-02-10 12:10:31 +0100

    Code cleanup.

--- lib/repository/api/src/main/java/com/sebster/repository/api/properties/orders/PropertyOrder.java
+++ lib/repository/api/src/main/java/com/sebster/repository/api/properties/orders/PropertyOrder.java
@@ -1,28 +1,26 @@
 package com.sebster.repository.api.properties.orders;
 
 import static java.util.Comparator.naturalOrder;
-import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PRIVATE;
 
 import java.util.Comparator;
 
-import com.sebster.repository.api.properties.Property;
 import com.sebster.repository.api.orders.Order;
+import com.sebster.repository.api.properties.Property;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.Value;
 
 @Value
-@AllArgsConstructor(access = PACKAGE)
+@AllArgsConstructor(access = PRIVATE)
 public class PropertyOrder<T, V extends Comparable<? super V>> implements Order<T> {
 
 	@NonNull Property<T, V> property;
 
 	@Override
 	public int compare(@NonNull T a, @NonNull T b) {
-		V va = property.getValue(a);
-		V vb = property.getValue(b);
 		Comparator<V> valueComparator = naturalOrder();
-		return valueComparator.compare(va, vb);
+		return valueComparator.compare(property.getValue(a), property.getValue(b));
 	}
 
 	public static <T, V extends Comparable<? super V>> PropertyOrder<T, V> orderBy(@NonNull Property<T, V> property) {
