fcf7be63b01f1badccaf4e3c869ef9df5b7e2dd3
Sebastiaan van Erk <sebster@sebster.com>
2020-02-11 11:01:45 +0100

    Fixed And specification adapter.

--- lib/repository/jpa/src/main/java/com/sebster/repository/jpa/specifications/JpaAndSpecificationAdapter.java
+++ lib/repository/jpa/src/main/java/com/sebster/repository/jpa/specifications/JpaAndSpecificationAdapter.java
@@ -1,4 +1,4 @@
-package com.sebster.repositandy.jpa.specifications;
+package com.sebster.repository.jpa.specifications;
 
 import javax.persistence.criteria.CommonAbstractCriteria;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -8,16 +8,14 @@ import javax.persistence.criteria.Root;
 import org.springframework.stereotype.Component;
 
 import com.sebster.repository.api.specifications.And;
-import com.sebster.repository.api.specifications.Or;
 import com.sebster.repository.api.specifications.Specification;
-import com.sebster.repository.jpa.specifications.AbstractJpaSpecificationAdapter;
 
 @Component
 public class JpaAndSpecificationAdapter<T> extends AbstractJpaSpecificationAdapter<T> {
 
 	@Override
 	public boolean canAdapt(Specification<?> spec) {
-		return spec instanceof Or;
+		return spec instanceof And;
 	}
 
 	@Override
