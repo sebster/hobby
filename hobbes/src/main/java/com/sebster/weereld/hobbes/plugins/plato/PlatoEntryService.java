5878ebd70b484ae762b6af33db3cd4d428b3902b
Harmen Renkema <harmen@harmenrenkema.nl>
2020-02-22 13:58:40 +0100

    replace usage of deprecated class

--- hobbes/src/main/java/com/sebster/weereld/hobbes/plugins/plato/PlatoEntryService.java
+++ hobbes/src/main/java/com/sebster/weereld/hobbes/plugins/plato/PlatoEntryService.java
@@ -10,7 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
