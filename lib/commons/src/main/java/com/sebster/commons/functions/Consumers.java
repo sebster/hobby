7b8d7d701f4b562d6b2d75bda211d3b3e88aedb2
Sebastiaan van Erk <sebster@sebster.com>
2020-02-27 09:04:11 +0100

    Added some utility functions for working with functions/consumers.

---
+++ lib/commons/src/main/java/com/sebster/commons/functions/Consumers.java
@@ -0,0 +1,40 @@
+package com.sebster.commons.functions;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * Utility functions for manipulating {@link Consumer}s.
+ */
+@AllArgsConstructor(access = PRIVATE)
+public class Consumers {
+
+	/**
+	 * Convert a bi-consumer to function returning a consumer by curring the specified bi-consumer.
+	 */
+	public static <U, V> Function<U, Consumer<V>> curry(@NonNull BiConsumer<U, V> biConsumer) {
+		return u -> v -> biConsumer.accept(u, v);
+	}
+
+	/**
+	 * Convert a function returning a consumer to a bi-consumer by uncurrying.
+	 */
+	public static <U, V> BiConsumer<U, V> uncurry(@NonNull Function<U, Consumer<V>> function) {
+		return (u, v) -> function.apply(u).accept(v);
+	}
+
+	/**
+	 * Return a function which converts a consumer to a new consumer which transforms the argument to be consumed with the specified
+	 * transform function first.
+	 */
+	public static <U, V> Function<Consumer<U>, Consumer<V>> map(@NonNull Function<? super V, ? extends U> transform) {
+		return uConsumer -> v -> uConsumer.accept(transform.apply(v));
+	}
+
+}
---
+++ lib/commons/src/main/java/com/sebster/commons/functions/Functions.java
@@ -0,0 +1,40 @@
+package com.sebster.commons.functions;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * Utility functions for manipulating {@link Function}s.
+ */
+@AllArgsConstructor(access = PRIVATE)
+public class Functions {
+
+	/**
+	 * Make the specified function null safe by mapping {@code null} to {@code null} and delegating to the specified function for
+	 * non-null arguments.
+	 */
+	public static <U, V> Function<U, V> nullSafe(@NonNull Function<U, V> function) {
+		return u -> u != null ? function.apply(u) : null;
+	}
+
+	/**
+	 * Return a function which unwraps an optional to the value if present and to {@code null} if it is empty.
+	 */
+	public static <U> Function<Optional<U>, U> unwrapOptional() {
+		return optional -> optional.orElse(null);
+	}
+
+	/**
+	 * Convert a function which returns an optional to a function which returns the value of the optional if present and {@code
+	 * null} if it is empty.
+	 */
+	public static <T, V> Function<T, V> unwrapOptional(@NonNull Function<T, Optional<V>> optionalFunction) {
+		return optionalFunction.andThen(unwrapOptional());
+	}
+
+}
