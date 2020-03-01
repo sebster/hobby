8927b96038c62864671cfd026bfca87c82c09336
Harmen Renkema <harmen@harmenrenkema.nl>
2020-03-01 13:08:15 +0100

    plato citation generation based on full sentences with a target for number of characters

--- hobbes/src/main/java/com/sebster/weereld/hobbes/plugins/plato/PlatoEntryService.java
+++ hobbes/src/main/java/com/sebster/weereld/hobbes/plugins/plato/PlatoEntryService.java
@@ -2,7 +2,10 @@ package com.sebster.weereld.hobbes.plugins.plato;
 
 import static java.util.regex.Pattern.compile;
 
-import java.net.URI;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,11 +22,12 @@ public class PlatoEntryService {
 
 	private static final String PLATO_ENTRY_BASE_URL_STRING = "https://plato.stanford.edu/entries/";
 
-	private static final Pattern CITATION_PATTERN = compile("(?i)<div id=\"preamble\">\\n\\n<p>.*\\n([^.]*.[^.]*.)");
+	private static final Pattern PREAMBLE_PATTERN = compile("(?i)<div id=\"preamble\">(.+?)</div>");
 	private static final Pattern ENTRY_NAME_PATTERN = compile(
-			"(?i) {12}<li><a href=\"https://plato.stanford.edu/cgi-bin/encyclopedia/archinfo\\.cgi\\?entry=([\\-\\w]*)");
+			"(?i)<li><a href=\"https://plato.stanford.edu/cgi-bin/encyclopedia/archinfo\\.cgi\\?entry=([\\-\\w]*)");
 
 	private final PlatoWebClient webClient;
+	private final PlatoProperties properties;
 
 	String getCitationFromRandomEntry() {
 		Optional<String> randomEntry = webClient.getRandomEntry();
@@ -41,13 +45,50 @@ public class PlatoEntryService {
 	}
 
 	private Optional<String> quote(String entry) {
-		return firstMatch(entry, CITATION_PATTERN)
-				.map(citation -> citation
-						.replace(" \n", " ")
-						.replace("\n ", " ")
-						.replace("\n", " ")
-						.replaceAll("<[^>]*>", "")
-				);
+		return firstMatch(stripNewLines(entry), PREAMBLE_PATTERN)
+				.map(this::cleanupHtmlTagsAndWhiteSpace)
+				.map(this::composeQuote);
+	}
+
+	private String composeQuote(String preamble) {
+		List<String> sentences = getSentences(preamble);
+		StringBuilder quote = new StringBuilder();
+
+		int target = properties.getQuoteCharactersTarget();
+		for (String sentence : sentences) {
+			if (Math.abs(target - quote.length()) < Math.abs(target - (quote.length() + sentence.length()))) {
+				break;
+			}
+			quote.append(sentence);
+		}
+
+		return quote.toString().trim();
+	}
+
+	private String stripNewLines(String string) {
+		return string
+				.replace(" \n", " ")
+				.replace("\n ", " ")
+				.replace("\n", " ");
+	}
+
+	private String cleanupHtmlTagsAndWhiteSpace(String string) {
+		return string
+				.replaceAll("<[^>]*>", "")
+				.replace("\t", "")
+				.trim();
+	}
+
+	private List<String> getSentences(String text) {
+		List<String> sentences = new ArrayList<>();
+		BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+		iterator.setText(text);
+
+		int start = iterator.first();
+		for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next()) {
+			sentences.add(text.substring(start, end));
+		}
+		return sentences;
 	}
 
 	private Optional<String> entryLink(String entry) {
