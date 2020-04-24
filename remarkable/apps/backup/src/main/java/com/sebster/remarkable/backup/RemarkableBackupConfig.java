aca70329ccb98fd7b16b4d5b777a73a09dc51151
Sebastiaan van Erk <sebster@sebster.com>
2020-04-24 08:36:04 +0200

    Configure object mapper in code.

--- remarkable/apps/backup/src/main/java/com/sebster/remarkable/backup/RemarkableBackupConfig.java
+++ remarkable/apps/backup/src/main/java/com/sebster/remarkable/backup/RemarkableBackupConfig.java
@@ -3,28 +3,30 @@ package com.sebster.remarkable.backup;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sebster.remarkable.backup.domain.RemarkableBackupService;
 import com.sebster.remarkable.backup.domain.RemarkableBackupStorageService;
 import com.sebster.remarkable.backup.infrastructure.FileSystemRemarkableBackupStorageService;
-import com.sebster.remarkable.cloudapi.RemarkableClientManager;
 
 @Configuration
 @EnableConfigurationProperties(RemarkableBackupProperties.class)
 public class RemarkableBackupConfig {
 
 	@Bean
-	public RemarkableBackupStorageService storageService(RemarkableBackupProperties properties, ObjectMapper objectMapper) {
-		return new FileSystemRemarkableBackupStorageService(properties.getLocation(), objectMapper);
+	public RemarkableBackupStorageService storageService(
+			RemarkableBackupProperties properties,
+			Jackson2ObjectMapperBuilder objectMapperBuilder
+	) {
+		return new FileSystemRemarkableBackupStorageService(
+				properties.getLocation(),
+				objectMapperBuilder.indentOutput(true).build()
+		);
 	}
 
 	@Bean
-	public RemarkableBackupService remarkableBackupService(
-			RemarkableClientManager clientManager,
-			RemarkableBackupStorageService storageService
-	) {
-		return new RemarkableBackupService(clientManager, storageService);
+	public RemarkableBackupService remarkableBackupService(RemarkableBackupStorageService storageService) {
+		return new RemarkableBackupService(storageService);
 	}
 
 }
