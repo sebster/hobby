6c4c03d7e540c872d673d59fd887a071faf71d59
Sebastiaan van Erk <sebster@sebster.com>
2020-04-24 08:54:30 +0200

    Use spring ApplicationArgs to skip option arguments.

--- remarkable/apps/backup/src/main/java/com/sebster/remarkable/backup/RemarkableBackup.java
+++ remarkable/apps/backup/src/main/java/com/sebster/remarkable/backup/RemarkableBackup.java
@@ -4,9 +4,9 @@ import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
-import java.util.stream.Stream;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,34 +18,33 @@ import lombok.NonNull;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class RemarkableBackup implements CommandLineRunner {
+public class RemarkableBackup implements ApplicationRunner {
 
 	private final @NonNull RemarkableClientManager clientManager;
 	private final @NonNull RemarkableBackupProperties backupProperties;
 	private final @NonNull RemarkableBackupService backupService;
 
 	@Override
-	public void run(String... args) {
+	public void run(ApplicationArguments args) {
 		getClients(args).forEach(client -> backupService.backup(client, backupProperties.getType()));
 	}
 
-	public List<RemarkableClient> getClients(String... args) {
+	public List<RemarkableClient> getClients(ApplicationArguments args) {
 		// Arguments override properties.
-		if (args.length > 0) {
-			return Stream.of(args).map(clientManager::getClient).collect(toList());
+		if (args.getNonOptionArgs().size() > 0) {
+			return args.getNonOptionArgs().stream().map(clientManager::getClient).collect(toList());
 		}
 
-		// Property overrides client config file.
+		// Property overrides client config.
 		if (backupProperties.getClientId() != null) {
 			return singletonList(clientManager.getClient(backupProperties.getClientId()));
 		}
 
-		// Default to the client config file.
+		// Default to the client config.
 		return clientManager.listClients();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RemarkableBackup.class, args);
 	}
-
 }
